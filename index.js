b99118dc2a72415e037f843ae0e556861aeb8938
Spencer <spencerjohngreat@gmail.com>
2022-08-01 09:04:26 +0100

    adjusted the index.js file

--- index.js
+++ index.js
@@ -6,9 +6,13 @@
 <body>
 
 <h1>This is a Heading</h1>
+<<<<<<< HEAD
 nnbdbd
 heheh
 <h2> This a heading H2 tag</h2>
+=======
+this is somwthing
+>>>>>>> 28ff607 (added something)
 <p>This is a paragraph.</p>
 
 </body>
