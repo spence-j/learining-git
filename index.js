17b09759e01113589e26d64d33e2e1efd8fc3310
Spencer <spencerjohngreat@gmail.com>
2022-08-01 09:13:33 +0100

    index.js

--- index.js
+++ index.js
@@ -6,13 +6,10 @@
 <body>
 
 <h1>This is a Heading</h1>
-<<<<<<< HEAD
 nnbdbd
 heheh
 <h2> This a heading H2 tag</h2>
-=======
 this is somwthing
->>>>>>> 28ff607 (added something)
 <p>This is a paragraph.</p>
 
 </body>
